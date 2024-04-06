94df8d17b384110aca31c34ac4a3798e2c1bf9c7
unknown <martiveselinov@gmail.com>
2024-04-07 00:58:23 +0300

    add secret

--- src/main/java/com/ninjas/gig/security/SecurityConstants.java
+++ src/main/java/com/ninjas/gig/security/SecurityConstants.java
@@ -1,4 +1,6 @@
 package com.ninjas.gig.security;
 
 public class SecurityConstants {
+    public static final long JWT_EXPIRATION = 70000;
+    public static final String JWT_SECRET = "SOME_SECRET_TO_ENCODE_THE_TOKEN";
 }
