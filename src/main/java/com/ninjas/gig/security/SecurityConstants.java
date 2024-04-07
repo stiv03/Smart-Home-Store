cbd93e19839a037165d36035d15b2b261867bb84
unknown <martiveselinov@gmail.com>
2024-04-07 13:40:36 +0300

    create random secret key

--- src/main/java/com/ninjas/gig/security/SecurityConstants.java
+++ src/main/java/com/ninjas/gig/security/SecurityConstants.java
@@ -1,6 +1,19 @@
 package com.ninjas.gig.security;
 
+import java.security.SecureRandom;
+import java.util.Base64;
+
 public class SecurityConstants {
     public static final long JWT_EXPIRATION = 70000;
-    public static final String JWT_SECRET = "SOME_SECRET_TO_ENCODE_THE_TOKEN";
+    public static final String JWT_SECRET;
+
+    static {
+        int keyLength = 256;
+
+        byte[] keyBytes = new byte[keyLength / 8];
+        SecureRandom secureRandom = new SecureRandom();
+        secureRandom.nextBytes(keyBytes);
+
+        JWT_SECRET = Base64.getEncoder().encodeToString(keyBytes);
+    }
 }
