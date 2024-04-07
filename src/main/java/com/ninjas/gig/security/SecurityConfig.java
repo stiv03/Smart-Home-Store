9d085c6a6b6596529c37955034c8f4a1f5840804
unknown <martiveselinov@gmail.com>
2024-04-07 11:20:31 +0300

    add jwt filter to chain

--- src/main/java/com/ninjas/gig/security/SecurityConfig.java
+++ src/main/java/com/ninjas/gig/security/SecurityConfig.java
@@ -18,6 +18,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -44,7 +45,7 @@ public class SecurityConfig {
                             .requestMatchers("/**").permitAll()
                             .anyRequest().authenticated()).httpBasic(Customizer.withDefaults());
             http.httpBasic(withDefaults());
-
+            http.addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
              return http.build();
     }
 
@@ -56,6 +57,11 @@ public class SecurityConfig {
     @Bean
     PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public JWTAuthenticationFilter jwtAuthenticationFilter(){
+        return new JWTAuthenticationFilter();
     }
 
 
