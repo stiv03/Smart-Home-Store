346f5ff5268a891927aea178b82897b2fd9b4533
unknown <martiveselinov@gmail.com>
2024-04-07 12:06:17 +0300

    security configs

--- src/main/java/com/ninjas/gig/security/JWTGenerator.java
+++ src/main/java/com/ninjas/gig/security/JWTGenerator.java
@@ -20,7 +20,7 @@ public class JWTGenerator {
                 .setSubject(username)
                 .setIssuedAt(new Date())
                 .setExpiration(expireDate)
-                .signWith(SignatureAlgorithm.ES512, SecurityConstants.JWT_SECRET)
+                .signWith(SignatureAlgorithm.HS512, SecurityConstants.JWT_SECRET)
                 .compact();
 
         return token;
