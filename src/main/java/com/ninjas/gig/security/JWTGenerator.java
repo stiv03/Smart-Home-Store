cfd083c082ff22659e01b6b1ffa2c09168c33f24
unknown <martiveselinov@gmail.com>
2024-04-13 20:13:56 +0300

    remove comented code

--- src/main/java/com/ninjas/gig/security/JWTGenerator.java
+++ src/main/java/com/ninjas/gig/security/JWTGenerator.java
@@ -42,19 +42,6 @@ public class JWTGenerator {
                 .compact();
     }
 
-//    public boolean isTokenValid(String token, UserDetails userDetails){
-//        final String username = getUsernameFromJWT(token);
-//        return (username.equals(userDetails.getUsername())) && !isTokenExpired(token);
-//    }
-//
-//    private boolean isTokenExpired(String token){
-//        return extractExpiration(token).before(new Date());
-//    }
-//
-//    private Date extractExpiration(String token) {
-//        return extractClaim(token, Claims::getExpiration);
-//    }
-
     public String getUsernameFromJWT(String token) {
         return extractClaim(token, Claims::getSubject);
     }
